3f6b20e623684d7612b783d117481aff6618d3da
Nguyen Duc Kien Trung <teo171103@gmail.com>
2024-08-16 00:00:14 +0700

    Add create new category

--- Models/Category.cs
+++ Models/Category.cs
@@ -6,6 +6,6 @@ namespace LibraryProject.Models
     {
         [Key]
         public int CategoryId { get; set; }
-        public string? CategoryName { get; set; }
+        public string CategoryName { get; set; }
     }
 }
